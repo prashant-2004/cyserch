9351b1da40ff13d1d801c548c9b3b32a08256c08
Pranav <pranavshelke4@gmail.com>
2024-04-20 16:12:02 +0530

    Add WhyChooseUs component to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Hero from "./components/home/hero";
 import SecendCards from "./components/home/secendCards";
+import WhyChooseUs from "./components/home/whyChooseUs";
 import Certificate from "./components/home/certificate";
 import Blog from "./components/home/blog";
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
       <div className="container mx-auto px-4"></div>
       <Hero />
       <SecendCards />
+      <WhyChooseUs />
       <Blog />
       <Certificate />
     </>
