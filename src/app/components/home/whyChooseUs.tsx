ca085fff8c3652cb54aa2c8b94455d31d0d4a87b
Pranav <pranavshelke4@gmail.com>
2024-04-21 12:07:12 +0530

    Update WhyChooseUs component in home page and modify global CSS styles

--- src/app/components/home/whyChooseUs.tsx
+++ src/app/components/home/whyChooseUs.tsx
@@ -1,14 +1,41 @@
+import Image from "next/image";
+import done from "../../../../public/icons/done.png";
+import hacker from "../../../../public/images/young-hacker.png";
+
 export default function WhyChooseUs() {
   return (
     <div className="WhyChooseUs">
       <div className="div-1">
-        <h3 className="text-white text-[1.4rem] font-semibold">
-          Security Services
+        <span className="text-[#19FFDB] text-[1.37rem] font-medium mb-4">
+          Why Choose Us
+        </span>
+        <h3 className="text-white text-[2.18rem] font-semibold">
+          Provide Advanced Security for Advanced Threat
         </h3>
+        <p className="text-[#A39D9D] text-[1.2rem] font-light mt-6">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod enim tempor incididunt ut labore et dolore magna aliqua. Ut
+          enim ad minim, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip
+        </p>
+        <ul className="mt-6">
+          <li className="flex items-center text-[#A39D9D] text-[1.2rem] font-light">
+            <Image src={done} alt="done" className="mr-3" /> Content Delivery
+            Network
+          </li>
+          <li className="flex items-center text-[#A39D9D] text-[1.2rem] font-light">
+            <Image src={done} alt="done" className="mr-3" /> Malware Detection
+            Removal
+          </li>
+          <li className="flex items-center text-[#A39D9D] text-[1.2rem] font-light">
+            <Image src={done} alt="done" className="mr-3" /> 24/7 Support And
+            Remote Admit
+          </li>
+        </ul>
       </div>
 
       <div className="div-2">
-        dsdf
+        <Image src={hacker} alt="hacker" />
       </div>
     </div>
   );
