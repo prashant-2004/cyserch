26a8aecf9943b2014a42324cba044c58cadebf98
pranav123451 <pranavjadhav432@gmail.com>
2024-04-23 21:15:57 +0530

    Refactor CourseCard component in TrainingPage and coursesData.tsx

--- src/app/components/training/CourseCard.tsx
+++ src/app/components/training/CourseCard.tsx
@@ -1,21 +1,22 @@
 import React from "react";
+import Image from "next/image";
 import Link from "next/link";
 
 const CourseCard = ({ course }: { course: any }) => {
   return (
     <div className="course-card border border-gray-300 rounded-lg shadow-md p-4 w-full sm:w-[20rem] md:w-[25rem]">
-      <img src={course.image} alt={course.title} className="mb-4 rounded-md" />
-      <div className="mb-2 text-gray-600">{course.category}</div>
-      <h2 className="text-xl font-bold mb-2">{course.title}</h2>
-      <div className="mb-4">{course.description}</div>
+      <Image width={400} height={100} src={course.image} alt={course.title} className="mb-4 rounded-md" />
+      <div className="mb-2 text-slate-200">{course.category}</div>
+      <h2 className="text-xl text-white font-bold mb-2">{course.title}</h2>
+      <div className="mb-4 text-slate-400">{course.description}</div>
       <div className="flex items-center justify-between">
-        <div className="text-gray-600">{course.duration}</div>
-        <div className="flex items-center">
-          <span className="text-yellow-400 mr-1">&#9733;</span> {course.rating}
+        <div className=" text-slate-400">{course.duration}</div>
+        <div className="flex items-center text-white">
+          <span className="text-yellow-400 mr-1 ">&#9733;</span> {course.rating}
         </div>
       </div>
       <Link href={`/course/${course.link}`}>
-        <div className="know-more-button bg-blue-500 text-white py-2 px-4 rounded-md mt-4 cursor-pointer">
+        <div className="know-more-button bg-blue-500 text-slate-200 py-2 px-4 rounded-md mt-4 cursor-pointer">
           Know More
         </div>
       </Link>
--- src/app/components/training/coursesData.tsx
+++ src/app/components/training/coursesData.tsx
@@ -1,81 +1,15 @@
-import { link } from "fs";
-
 const courses = [
-  {
-    id: 1,
-    title: "Course Title 1",
-    category: "Category 1",
-    image: "/course1.jpg",
-    description: "Course description here.",
-    duration: "Total duration: 10 hours",
-    rating: "Rating: 4.5",
-    link: "course-title-1",
-  },
-  {
-    id: 2,
-    title: "Course Title 1",
-    category: "Category 1",
-    image: "/course1.jpg",
-    description: "Course description here.",
-    duration: "Total duration: 10 hours",
-    rating: "Rating: 4.5",
-    link: "course-title-1",
-  },
+    {
+        id: 1,
+        title: "Course Title 1",
+        category: "Category 1",
+        image: "/images/card1.png",
+        description: "Course description here.",
+        duration: "Total duration: 10 hours",
+        rating: "Rating: 4.5",
+        link: "course-title-1",
+    },
 
-  {
-    id: 3,
-    title: "Course Title 1",
-    category: "Category 1",
-    image: "/course1.jpg",
-    description: "Course description here.",
-    duration: "Total duration: 10 hours",
-    rating: "Rating: 4.5",
-    link: "course-title-1",
-  },
-
-  {
-    id: 4,
-    title: "Course Title 1",
-    category: "Category 1",
-    image: "/course1.jpg",
-    description: "Course description here.",
-    duration: "Total duration: 10 hours",
-    rating: "Rating: 4.5",
-    link: "course-title-1",
-  },
-
-  {
-    id: 3,
-    title: "Course Title 1",
-    category: "Category 1",
-    image: "/course1.jpg",
-    description: "Course description here.",
-    duration: "Total duration: 10 hours",
-    rating: "Rating: 4.5",
-    link: "course-title-1",
-  },
-
-  {
-    id: 3,
-    title: "Course Title 1",
-    category: "Category 1",
-    image: "/course1.jpg",
-    description: "Course description here.",
-    duration: "Total duration: 10 hours",
-    rating: "Rating: 4.5",
-    link: "course-title-1",
-  },
-
-  {
-    id: 3,
-    title: "Course Title 1",
-    category: "Category 1",
-    image: "/course1.jpg",
-    description: "Course description here.",
-    duration: "Total duration: 10 hours",
-    rating: "Rating: 4.5",
-    link: "course-title-1",
-  },
 ];
 
 export default courses;
