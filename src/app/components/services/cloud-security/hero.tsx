a816028bb40ea90cf19de2e8bdb79795f40b13bd
Pranav <pranavshelke4@gmail.com>
2024-04-25 20:41:10 +0530

    Update client page title and add cloud security components

---
+++ src/app/components/services/cloud-security/hero.tsx
@@ -0,0 +1,13 @@
+export default function CloudSecurityHero() {
+  return (
+    <div className="cloud-hero">
+      <div className="cloud-hero-img"></div>
+      <div className="cloud-hero__content">
+        <h1 className="text-[#19FFDB] text-[3.30rem] font-bold">Cloud Security</h1>
+        <p className="text-[#A39D9D] text-[1.2rem] font-light mt-2">
+        Better business necessitates better security with Cloud Security Testing.
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/services/cloud-security/page.tsx
@@ -0,0 +1,17 @@
+import CloudSecurityHero from "@/app/components/services/cloud-security/hero";
+import CloudSecurityOverview from "@/app/components/services/cloud-security/overview";
+import CloudSecurityMethodology from "@/app/components/services/cloud-security/methodology";
+import CloudSecurityBenefits from "@/app/components/services/cloud-security/benefits";
+import CloudSecurityClients from "@/app/components/services/cloud-security/clients";
+
+export default function CloudSecurityPage() {
+  return (
+    <>
+      <CloudSecurityHero />
+      <CloudSecurityOverview />
+      <CloudSecurityMethodology />
+      <CloudSecurityClients />
+      <CloudSecurityBenefits />
+    </>
+  );
+}
