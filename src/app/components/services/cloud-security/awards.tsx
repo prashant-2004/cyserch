58b15d7edb2e836b474f88b3c54f5ab01e62d2e4
pranav123451 <pranavjadhav432@gmail.com>
2024-04-27 02:08:34 +0530

    Update Cloud Security components and fix navbar

--- src/app/components/services/cloud-security/awards.tsx
+++ src/app/components/services/cloud-security/awards.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
-import Award_1 from "../..//../../../public/images/award_1.jpeg";
-import Award_2 from "../..//../../../public/images/award_2.jpeg";
+import Award_1 from "../../../../../public/images/award_1.jpeg";
+import Award_2 from "../../../../../public/images/award_2.jpeg";
 
-export default function CybersecurityAwards(){
-    return(
-        <div className="flex flex-wrap h-96 -mx-4 gap-1rem">
+export default function CybersecurityAwards() {
+  return (
+    <div className="flex flex-wrap h-96 px-4 sm:px-6 md:px-8 lg:px-[9rem] py-4 sm:py-6 md:py-[3rem] space-x-4 md:space-x-8">
       {/* Column for Text */}
-      <div className="w-full md:w-1/3 px-4 flex items-center">
-        <div className="text-xl font-bold py-6 text-white">Awards and Recognition</div>
+      <div className="w-full md:w-1/3 px-4 flex items-start">
+        <div className="text-xl sm:text-[2.18rem] font-bold py-6 text-white">
+          Awards and Recognition
+        </div>
       </div>
 
       {/* Columns for Images with Text Overlay */}
@@ -19,8 +21,11 @@ export default function CybersecurityAwards(){
           objectFit="cover"
           className="rounded-lg"
         />
-        <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center">
-          <p className="text-white text-xl font-semibold">Cyserch recognized as a Major Contender” in Everest Group’s Managed Detection and Response (MDR) Services PEAK Matrix® Assessment 2023®</p>
+        <div className="absolute inset-0 bg-black bg-opacity-60 flex items-center justify-center">
+          <p className="text-white text-lg sm:text-xl px-8 font-semibold">
+            Cyserch recognized as a Major Contender” in Everest Group’s Managed
+            Detection and Response (MDR) Services PEAK Matrix® Assessment 2023®
+          </p>
         </div>
       </div>
 
@@ -32,10 +37,13 @@ export default function CybersecurityAwards(){
           objectFit="cover"
           className="rounded-lg"
         />
-        <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center">
-          <p className="text-white text-xl font-semibold">Cyserch recognized as a Major Contender” in Everest Group’s Identity & Access Management (IAM) Services Peak Matrix ® Assessment 2023</p>
+        <div className="absolute inset-0 bg-black bg-opacity-60 flex items-center justify-center">
+          <p className="text-white text-lg sm:text-xl px-8 font-semibold">
+            Cyserch recognized as a Major Contender” in Everest Group’s Identity
+            & Access Management (IAM) Services Peak Matrix ® Assessment 2023
+          </p>
         </div>
       </div>
     </div>
-    );
+  );
 }
--- src/app/components/services/cloud-security/overview.tsx
+++ src/app/components/services/cloud-security/overview.tsx
@@ -1,14 +1,14 @@
 export default function CloudSecurityOverview() {
   return (
-    <div className="text-center px-[9rem] py-[3rem]">
-      <h1 className="text-white text-[2.18rem] font-semibold mt-3">Overview : Cyber Defense Resiliency Service</h1>
-      <h3 className="text-gray-400 font-bold text-[1.18rem] mt-3">Next-Gen Cybersecurity Solutions: Empowering Cyber Defense Resiliency</h3>
-      <p className="text-[#A39D9D] text-[1.2rem] font-light mt-4">
+    <div className="text-center px-4 sm:px-6 md:px-8 lg:px-[9rem] py-4 sm:py-6 md:py-[3rem]">
+      <h1 className="text-white text-xl sm:text-2xl md:text-[2.18rem] font-semibold mt-3">Overview : Cyber Defense Resiliency Service</h1>
+      <h3 className="text-gray-400 font-bold text-lg sm:text-xl md:text-[1.18rem] mt-3">Next-Gen Cybersecurity Solutions: Empowering Cyber Defense Resiliency</h3>
+      <p className="text-[#A39D9D] text-base sm:text-lg md:text-[1.2rem] font-light mt-4">
       Digital transformations bring along tremendous benefits, but not without threats to the privacy and security of crucial enterprise data. In present times, cyberattacks are rampant, and new-age cyber threats require dexterous strategies. Therefore, enterprises need to develop cyber defense resilience to counter such incidents.
       </p>
-      <p className="text-[#A39D9D] text-[1.2rem] font-light mt-4">At Cyserch, we follow a customer-centric approach to create cybersecurity solutions that help our clients build resilient enterprises. We offer platform-based enterprise cybersecurity solutions, which are proactive, prescriptive, and cognitively autonomous.
+      <p className="text-[#A39D9D] text-base sm:text-lg md:text-[1.2rem] font-light mt-4">At Cyserch, we follow a customer-centric approach to create cybersecurity solutions that help our clients build resilient enterprises. We offer platform-based enterprise cybersecurity solutions, which are proactive, prescriptive, and cognitively autonomous.
       </p>
-      <p className="text-[#A39D9D] text-[1.2rem] font-light mt-4">
+      <p className="text-[#A39D9D] text-base sm:text-lg md:text-[1.2rem] font-light mt-4">
         As a security transformation partner, we have helped over 220 clients in more than 30 countries build personalized security roadmaps aligned to their digital transformation goals. We create digital cyber defense capabilities to solve some of the most complex challenges and secure digital transformations at scale.
       </p>
     </div>
